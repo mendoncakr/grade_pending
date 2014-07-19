1437a43f304f441ed341261c579eab96cf2e93bf
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-18 21:45:20 -0400

    blank interactive map on page

--- grade_pending/app/models/restaurant.rb
+++ grade_pending/app/models/restaurant.rb
@@ -1,2 +1,11 @@
 class Restaurant < ActiveRecord::Base
+  # def self.create_address
+  #   "#{self.building}" + " #{self.street} " + "#{self.zipcode}"
+  # end
+
+  # address: self.create_address
+
+  # before_save
+  geocoded_by :address
+  after_validation :geocode
 end
