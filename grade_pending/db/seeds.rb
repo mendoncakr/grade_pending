cdbb7d1f5b5a03c7b108ed9940dd5ec1e8e607d5
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-20 13:10:33 -0400

    merge conflict in seeds fixed

--- grade_pending/db/seeds.rb
+++ grade_pending/db/seeds.rb
@@ -11,12 +11,6 @@ file = File.expand_path('../WebExtract2.csv', __FILE__)
 array = []
 CSV.parse(File.open(file, 'r:iso-8859-1:utf-8'){|f| f.read}, col_sep: ',', headers: true) do |row|
    row = row.to_hash
-<<<<<<< HEAD
-   #  row.each_pair do |key, value|
-   #      unless value.class == NilClass
-   #      value.strip
-   #  end
-   # end
   Restaurant.create(row) if row["zipcode"] == "10005"
 end
 
@@ -35,43 +29,3 @@ end
 #     currentgrade: "B",
 #     gradedate: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
 #     recorddate: Time.now.strftime("%Y-%m-%d %H:%M:%S"))
-
-
-# row["camis"]
-# row["dba"]
-# row["camis"]
-# row["camis"]
-# row["camis"]
-# row["camis"]
-# row["camis"]
-# row["camis"]
-# row["camis"]
-# row["camis"]
-
-=======
-    row.each_pair do |key, value|
-       unless value.class == NilClass
-     value.gsub!(/\s\s+/, " ")
-    end
-   end
-  Restaurant.create(row)
-end
-
-# Restaurant.create(
-#     camis: "12345148",
-#     dba: "Sweet Eats",
-#     boro: "2",
-#     building: "586",
-#     street: "Broad Ave",
-#     zipcode: "12341",
-#     phone: "7181230567",
-#     cuisine: "05",
-#     inspdate: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
-#     action: "F",
-#     violcode: "06A",
-#     score: "10",
-#     currentgrade: "B",
-#     gradedate: Time.now.strftime("%Y-%m-%d %H:%M:%S"),
-#     recorddate: Time.now.strftime("%Y-%m-%d %H:%M:%S"))
->>>>>>> 735133814a6f49ddc23f0d700789114c9ee4c8b5
-
