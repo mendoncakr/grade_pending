683554e2fc712411187046cd866c19f28747d103
Joe <jdcohen9@gmail.com>
2014-07-19 22:04:05 -0400

    update model

--- grade_pending/app/models/restaurant.rb
+++ grade_pending/app/models/restaurant.rb
@@ -17,9 +17,22 @@ class Restaurant < ActiveRecord::Base
   validates :recorddate, presence: true
 
 
+  attr_accessor :address, :latitude, :longitude
 
-  def address
-    self.building  + " " + self.street + " " + self.zipcode
+
+  geocoded_by :address
+  after_validation :geocode
+
+  before_save :geocode
+  geocoded_by :address
+
+
+
+  def create_address
   end
 
+  def address
+   "#{self.building} " + " #{self.street} " + "#{self.zipcode}"
+  end
+  # before_save
 end
