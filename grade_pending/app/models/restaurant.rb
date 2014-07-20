a04106cadc69973e1c9c90dfab61e72ffcd3389e
Joe <jdcohen9@gmail.com>
2014-07-19 21:48:16 -0400

    all model tests working + modified restaurant model

--- grade_pending/app/models/restaurant.rb
+++ grade_pending/app/models/restaurant.rb
@@ -1,20 +1,25 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessor :address, :latitude, :longitude
+  validates :camis, presence: true
+  validates_uniqueness_of :camis
+  validates :dba, presence: true
+  validates :boro, presence: true
+  validates :building, presence: true
+  validates :street, presence: true
+  validates :zipcode, presence: true
+  validates :phone, presence: true
+  validates :cuisinecode, presence: true
+  validates :inspdate, presence: true
+  validates :action, presence: true
+  validates :violcode, presence: true
+  validates :score, presence: true
+  validates :currentgrade, presence: true
+  validates :gradedate, presence: true
+  validates :recorddate, presence: true
 
-  geocoded_by :address
-  after_validation :geocode
 
-  before_save :geocode
-  geocoded_by :address
-
-
-
-  def create_address
-  end
 
   def address
-   "#{self.building} " + " #{self.street} " + "#{self.zipcode}"
+    self.building  + " " + self.street + " " + self.zipcode
   end
-  # before_save
 
 end
