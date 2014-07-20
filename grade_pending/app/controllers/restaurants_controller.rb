7c59af98ab9e23af1600bf69bbf3265f536d5b36
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-07-20 19:30:07 -0400

    markers now show their respective inspection information

--- grade_pending/app/controllers/restaurants_controller.rb
+++ grade_pending/app/controllers/restaurants_controller.rb
@@ -1,19 +1,22 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.first(2)
+    
+    @restaurants = Restaurant.first(100)
 
-    @restaurants.each do |restaurant| 
-      @restaurant_info = {
-        "restaurant" => restaurant.dba,
-        "currentgrade" => restaurant.currentgrade,
-        "zipcode" => restaurant.zipcode,
-        "currentgrade" => restaurant.currentgrade
-      }
-      # restaurant.save_coordinates
-    end
+    # @restaurants.each do |restaurant| 
+    #   restaurant.restaurant_info = {
+    #     "restaurant" => restaurant.dba,
+    #     "currentgrade" => restaurant.currentgrade,
+    #     "zipcode" => restaurant.zipcode,
+    #     "currentgrade" => restaurant.currentgrade
+    #   }
+    #   # restaurant.save_coordinates
+    #   # sleep(1)
+    # end
 
 
     @hash = Gmaps4rails.build_markers(@restaurants) do |restaurant, marker|
+      @restaurant_info = restaurant.info
       marker.lat restaurant.coord1
       marker.lng restaurant.coord2
       marker.infowindow render_to_string(:partial => "/restaurants/info", :locals => { :object => @restaurant_info})
@@ -22,10 +25,6 @@ class RestaurantsController < ApplicationController
 
     # @restaurant_info = {:dba => @restaurant.dba,:current_grade=> @restaurant.currentgrade}
 
-  end
-
-  def show
-    @restaurant
   end
 
 end
