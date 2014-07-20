e5449823194b87d84a15038fb6115af20d379707
Brendan Susens-Jackson & Kenneth M <pair+bsusensjackson.mendoncakr@devbootcamp.com>
2014-07-20 19:53:24 -0400

    removed unnecessary code in addindex migration

--- grade_pending/db/migrate/20140720024410_add_index_to_restaurants.rb
+++ grade_pending/db/migrate/20140720024410_add_index_to_restaurants.rb
@@ -1,6 +1,5 @@
 class AddIndexToRestaurants < ActiveRecord::Migration
   def change
     add_index :restaurants, [:zipcode, :currentgrade]
-    # add_column :restaurants,  :add_index, :currentgrade
   end
 end
