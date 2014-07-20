7a7706e3bb85326878becd4a6088153e8cffd27d
Brendan Susens-Jackson & Kenneth M <pair+bsusensjackson.mendoncakr@devbootcamp.com>
2014-07-20 01:03:07 -0400

    lol

--- grade_pending/app/models/restaurant.rb
+++ grade_pending/app/models/restaurant.rb
@@ -1,13 +1,19 @@
 class Restaurant < ActiveRecord::Base
-  attr_accessor :address, :latitude, :longitude, :dba, :gmaps4rails_infowindow
-
+  attr_accessor :address, :latitude, :longitude, :dba
   geocoded_by :address
   after_validation :geocode
-
   before_save :geocode
   geocoded_by :address
+  validates_uniqueness_of: :dba
+
 
   def address
    "#{self.building} " + " #{self.street} " + "#{self.zipcode}"
+  end
+
+  def delete_dups
+    self.where() && (self.INSPDATE)
+
+    # Name is   and inspection date is most recent
   end
 end
---
+++ grade_pending/db/migrate/20140720024410_add_index_to_restaurants.rb
@@ -0,0 +1,9 @@
+class AddIndexToRestaurants < ActiveRecord::Migration
+  def change
+    add_column :restaurants, :add_index, :string
+    add_column :restaurants, :, :restaurants,
+    add_column :restaurants, :, :name,
+    add_column :restaurants, :, :unique
+    add_column :restaurants, :=, :string
+  end
+end
